beb811acc613f24813655d380d0cbae084828daa
samuelkyprianou <samuelkyprianou@gmail.com>
2020-02-05 13:02:05 +0000

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,23 @@
 class Blogger < ApplicationRecord
+has_many :posts
+has_many :destinations, through: :posts
+validates :name, uniqueness: true
+validates :bio, length: { minimum: 30 }
+validates :age, numericality: { greater_than: 0 }
+
+def blogger_likes
+    self.posts.reduce(0) { |sum, post| sum + post.likes }
+end
+
+def featured_most_liked_post
+    self.posts.max_by(&:likes)
+end
+
+def most_written_destinations(limit=3)
+    destinations
+      .group("destinations.id")
+      .order("count(destinations.id) DESC")
+      .limit(limit)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :bloggers
+  resources :posts do
+      post :like
+    end
+  resources :destinations
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
